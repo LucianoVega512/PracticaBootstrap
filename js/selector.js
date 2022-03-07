d7bdb682937133e9ec96e89dbbc16573f32c1e2f
Luciano <lucianomartin2015nqn@gmail.com>
2022-03-07 07:25:28 -0300

    Optimiza el archivo selector.js

--- js/selector.js
+++ js/selector.js
@@ -1,4 +1,8 @@
-function activo(btn){
+let menu1 = document.getElementById("menu1");
+let menu2 = document.getElementById("menu2");
+let menu3 = document.getElementById("menu3");
+
+function activo(btn) {
     let clase = btn.getAttribute("class");
 }
 
@@ -26,3 +30,20 @@ window.onload = () => {
         menu1.setAttribute("class", "btn btn-primary btn-lg my-3 mx-3");
     };
 };
+
+/*
+    <h1><span class="badge bg-primary mt-3">Login</span></h1>
+                <form>
+                    <div class="input-group my-3">
+                        <input type="text" class="form-control" placeholder="Nombre Usuario" aria-label="Usuario">
+                    </div>
+    
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Clave" aria-label="Clave">
+                    </div>
+    
+                    <div class="input-group mb-3">
+                        <input type="submit" class="form-control btn btn-primary" value="Enviar">
+                    </div>
+                </form>
+*/
